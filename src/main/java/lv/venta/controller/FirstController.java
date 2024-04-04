d1659734918150b87c297bc2d30930ea64ed1a98
Kelechi100 <kintucis6@gmail.com>
2024-04-04 11:01:48 +0300

    Add: the second controller

--- src/main/java/lv/venta/controller/FirstController.java
+++ src/main/java/lv/venta/controller/FirstController.java
@@ -1,16 +1,27 @@
 package lv.venta.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
+
+
 @Controller
 public class FirstController {
-	@GetMapping("/hello")
-	public String getHome() {
+	
+	@GetMapping("/hello")//localhost:8080/hello
+	public String getHello() {
 		System.out.println("Hello from Spring boot");
-		return "hello-page";
+		return "hello-page";//tiek paradita hello-page lapa
+	}
+	
+	@GetMapping("/hello/msg")//localhost:8080/hello/msg
+	public String getHelloMsg(Model model) {
+						//nosaukums, vertiba
+		model.addAttribute("mymsg","Zina no backend!! Hei!");
+		return "msg-page"; //tiek paradita msg-page lapa
 	}
 	
 }
