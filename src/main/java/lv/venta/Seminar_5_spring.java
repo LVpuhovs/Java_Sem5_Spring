11624f9391db2022915a1870414a759233f0f950
Kelechi100 <kintucis6@gmail.com>
2024-04-04 12:08:39 +0300

    Add: sviests

---
+++ src/main/java/lv/venta/Seminar_5_spring.java
@@ -0,0 +1,13 @@
+package lv.venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seminar_5_spring {
+
+public static void main(String[] args) {
+SpringApplication.run(Seminar_5_spring.class, args);
+}
+
+}
--- src/main/java/lv/venta/controller/FirstController.java
+++ src/main/java/lv/venta/controller/FirstController.java
@@ -1,9 +1,13 @@
 package lv.venta.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import lv.venta.model.Product;
 
 
 
@@ -22,6 +26,26 @@ public class FirstController {
 						//nosaukums, vertiba
 		model.addAttribute("mymsg","Zina no backend!! Hei!");
 		return "msg-page"; //tiek paradita msg-page lapa
+	}
+	
+	@GetMapping("/product/test")//localhost:8080/product/test
+	public String getProductTest(Model model) {
+		Product myProduct = new Product("Abols", "Sarkans", 0.99f, 5);
+		model.addAttribute("myobj", myProduct);
+		return "show-product-page"; //tiek paradita show-product-page lapa
+	}
+	
+	@GetMapping("/product/all")
+	public String getProductAll(Model model) {
+		Product myProduct1 = new Product("Abols", "Zals", 0.69f, 5);
+		Product myProduct2 = new Product("Zemene", "dzeltena", 0.25f, 3);
+		Product myProduct3 = new Product("Zars", "Bruns", 1.29f, 2);
+		ArrayList<Product> allProduct = new ArrayList<Product>();
+		allProduct.add(myProduct1);
+		allProduct.add(myProduct2);
+		allProduct.add(myProduct3);
+		model.addAttribute("mylist" ,allProduct);
+		return "show-product-all-page";
 	}
 	
 }
