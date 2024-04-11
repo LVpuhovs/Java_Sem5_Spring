d0d3afc8f170bdf999470f5f2ca01ef0f5b80763
Kelechi100 <kintucis6@gmail.com>
2024-04-11 09:44:45 +0300

    Error page

--- src/main/java/lv/venta/controller/FirstController.java
+++ src/main/java/lv/venta/controller/FirstController.java
@@ -1,10 +1,12 @@
 package lv.venta.controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import lv.venta.model.Product;
@@ -14,6 +16,10 @@ import lv.venta.model.Product;
 
 @Controller
 public class FirstController {
+	Product myProduct1 = new Product("Abols", "Zals", 0.69f, 5);
+	Product myProduct2 = new Product("Zemene", "dzeltena", 0.25f, 3);
+	Product myProduct3 = new Product("Zars", "Bruns", 1.29f, 2);
+	ArrayList<Product> allProduct = new ArrayList<>(Arrays.asList(myProduct1,myProduct2, myProduct3 ));
 	
 	@GetMapping("/hello")//localhost:8080/hello
 	public String getHello() {
@@ -35,12 +41,9 @@ public class FirstController {
 		return "show-product-page"; //tiek paradita show-product-page lapa
 	}
 	
-	@GetMapping("/product/all")
+	@GetMapping("/product/all")//localhost:8080/product/all
 	public String getProductAll(Model model) {
-		Product myProduct1 = new Product("Abols", "Zals", 0.69f, 5);
-		Product myProduct2 = new Product("Zemene", "dzeltena", 0.25f, 3);
-		Product myProduct3 = new Product("Zars", "Bruns", 1.29f, 2);
-		ArrayList<Product> allProduct = new ArrayList<Product>();
+		
 		allProduct.add(myProduct1);
 		allProduct.add(myProduct2);
 		allProduct.add(myProduct3);
@@ -48,4 +51,30 @@ public class FirstController {
 		return "show-product-all-page";
 	}
 	
+	
+	@GetMapping("/productone")//localhost:8080/productone?id=3
+	public String getProductOne(@RequestParam("id") int id, Model model) {
+		for (Product tempP: allProduct) {
+			if(tempP.getId() == id) {
+				model.addAttribute("myobj", tempP);
+				return "show-product-page";
+			}
+		}
+		
+		model.addAttribute("msg", "Wrong id");
+		return "error-page";
+	}
+	
+	@GetMapping("/product/all/{id}")//localhost:8080/product/all/2
+	public String getProductAllId(@PathVariable("id") int id, Model model) {
+		for (Product tempP: allProduct) {
+			if(tempP.getId() == id) {
+				model.addAttribute("myobj", tempP);
+				return "show-product-page";
+			}
+		}
+		
+		model.addAttribute("msg", "Wrong id");
+		return "error-page";
+	}
 }
