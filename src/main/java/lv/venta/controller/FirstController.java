97c84c090e91fcbcfc85dd89765cbaeb7a7a1cc1
Markuss Puhovs <s22puhomark@vea.lv>
2024-04-25 08:01:33 +0300

    delete

--- src/main/java/lv/venta/controller/FirstController.java
+++ src/main/java/lv/venta/controller/FirstController.java
@@ -100,6 +100,7 @@ public class FirstController  {
 	
 	@GetMapping("/product/insert")//localhost:8080/product/insert
 	public String getProductInsert(Model model) {
+		
 		model.addAttribute("product", new Product());
 		return "insert-product-page";
 	}
@@ -126,6 +127,55 @@ public class FirstController  {
 	public String getError() {
 		return "error-page";
 	}
+	
+	@GetMapping("/product/update") //localhost:8080/product/update?id=2
+	public String getProductUpateById(@RequestParam("id") int id, Model model ) {
+		
+		try {
+			Product product = crudService.retrieveById(id);
+			model.addAttribute("product", product);
+			model.addAttribute("id", id);
+			return "update-product-page";//tiek parādīta update-product-page.html lapa
+				
+		} catch (Exception e) {
+			model.addAttribute("msg", e.getMessage());
+			return "error-page"; // tiek parādīta error-page.html lapa
+		}
+		
+		
+	}
+	
+	@PostMapping("/product/update")
+	public String postProductInsert(@RequestParam("id") int id,  @Valid Product product, BindingResult result, Model model) {// iegūst redigēto produktu
+
+		if (result.hasErrors()) {
+			model.addAttribute("id", id);
+			return "update-product-page";
+		} else {
+
+			try {
+				crudService.updateById(id, product);
+				return "redirect:/product/all/" + id;
+			} catch (Exception e) {
+				return "redirect:/error";// pārlec uz citu endpointu
+			}
+		}
+
+	}
+	
+	@GetMapping("/product/delete") //localhost:8080/product/delete?id=2
+	public String getProductDeleteById(@RequestParam("id") int id, Model model) {
+		
+		try {
+			crudService.deleteById(id);
+			model.addAttribute("myobjs", crudService.retrieveAll());
+			return "show-product-all-page"; // tiek parādīta show-product-all-page.html lapa	
+		} catch (Exception e) {
+			model.addAttribute("msg", e.getMessage());
+			return "error-page"; // tiek parādīta error-page.html lapa
+		}
+	}
+	
 	//2. Izveido html lapu kurai bus 4 ievades lauki un submit poga
 	//3. izveido postmapping funkciju, kura nolasa Product, kas nak no html
 	// ar servisa palidzibu saglaba
