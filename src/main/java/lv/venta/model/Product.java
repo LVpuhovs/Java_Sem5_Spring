23da14452d1419e779fe84e2fb7bac1e079a3402
Markuss Puhovs <s22puhomark@vea.lv>
2024-04-25 11:51:23 +0300

    Update CRUD controller

--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -1,5 +1,12 @@
 package lv.venta.model;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
@@ -16,35 +23,40 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString
 //@AllArgsConstructor
+@Table(name = "ProductTable")
+@Entity
 public class Product {
-	private int id;
 	
-	private static int counter = 0;
+	@Column(name = "Id")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
 	
 	@NotNull
 	@Pattern(regexp = "[A-Z]{1}[a-z ]+", message = "Only letters and space")
 	@Size(min = 3, max = 20)
+	@Column(name = "Title")
 	private String title;
 	
 	@NotNull
 	@Pattern(regexp = "[A-Za-z ]+", message = "Only letters and space")
 	@Size(min = 4, max = 200)
+	@Column(name = "Description")
 	private String description;
 	
 	@Min(0)
 	@Max(10000)
+	@Column(name = "Price")
 	private float price;
 	
 	@Min(0)
 	@Max(100)
+	@Column(name = "Quantity")
 	private int quantity;
 	
-	public void setId() {
-		this.id = counter;
-		counter++;
-	}
+	
 	public Product(String title, String description, float price, int quantity) {
-		setId();
+		
 		setTitle(title);
 		setDescription(description);
 		setPrice(price);
