dde14031de8be3610e58615742a4701ef98d7deb
Ilja Avlass <epastsneeksiste@gmail.com>
2024-05-16 10:28:27 +0300

    pabeigts user and authority

--- src/main/java/lv/venta/model/MyAuthority.java
+++ src/main/java/lv/venta/model/MyAuthority.java
@@ -2,11 +2,17 @@ package lv.venta.model;
 
 
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -34,6 +40,29 @@ public class MyAuthority {
 	@Pattern(regexp = "[A-Z]{4,5}", message = "Only letters and numbers")
 	@Column(name = "Title")
 	private String title;
+	
+	@ManyToMany
+	@JoinTable(name = "UserAuthorityTable",
+	joinColumns = @JoinColumn(name = "Ida"),
+	inverseJoinColumns = @JoinColumn(name = "Idu"))
+	@ToString.Exclude
+	private Collection<MyUser> users = new ArrayList<MyUser>();
+	
+	
+	public void addAuthority(MyUser user) {
+		if(!users.contains(user))
+			users.add(user);
+	}
+	
+	public void remove (MyUser user) {
+		if(users.contains(user))
+			users.remove(user);
+	}
+	
+	public MyAuthority(String title)
+	{
+		setTitle(title);
+	}
 	
 	
 
--- src/main/java/lv/venta/model/MyUser.java
+++ src/main/java/lv/venta/model/MyUser.java
@@ -1,12 +1,18 @@
 package lv.venta.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,22 +22,50 @@ import lombok.ToString;
 @Setter
 @NoArgsConstructor
 @ToString
-@Table(name = "UserTable")
 @Entity
 public class MyUser {
+	
+	@Column(name = "Idu")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long idu;
+	
 	@NotNull
-	@Pattern(regexp = "[A-Za-z.]+", message = "Only letters and dot")
-	@Size(min = 4, max = 200)
+	@Pattern(regexp = "[A-Za-z.]+")
+	@Size(min = 3, max = 20)
 	@Column(name = "Username")
 	private String username;
 	
 	@NotNull
-	@Size(min = 6, max = 200)
+	@Size(min = 3, max = 20)
 	@Column(name = "Password")
 	private String password;
 	
-	public MyUser(String username, String password) {
+	@ManyToMany(mappedBy = "users", fetch = FetchType.EAGER)
+	private Collection<MyAuthority> authorities = new ArrayList<MyAuthority>();
+	
+	
+	public MyUser (String username, String password, MyAuthority ... auths) {
 		setUsername(username);
 		setPassword(password);
+		for(MyAuthority tempA: auths)
+			addAuthority(tempA);
 	}
+	
+	
+	public void addAuthority(MyAuthority authority) {
+		if(!authorities.contains(authority))
+			authorities.add(authority);
+	}
+	
+	public void removeAuthority(MyAuthority authority) {
+		if(authorities.contains(authority))
+			authorities.remove(authority);
+	}
+	
+	
+	
+	
+	
+	
 }
